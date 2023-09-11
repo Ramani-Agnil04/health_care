ea5b6a941247e3914ebea989f24b0a6695c23ec6
Ramani-Agnil04 <118161326+Ramani-Agnil04@users.noreply.github.com>
2023-09-11 10:46:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <span className='navbar'>
+        <img src='https://www.cubesimple.com/assets/images/svg/logo.svg' height='37' width='180' />
+      </span>
+      </header>
+      <body>
+        <div className='level'>
+        <p>
+            gfchgfhg
+            xrgcvhj
+            gcv juhi
+          </p>
+        </div>
+        <div className='report'>
+<span className='report-1 reports'>
+asdfghj
+</span>
+<span className='report-2 reports'>
+ASDFGHJ
+</span>
+<span className='report-3 reports'>
+pouytre
+</span>
+        </div>
+        <div className='table'>
+
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
